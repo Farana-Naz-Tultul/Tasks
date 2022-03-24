33aeb90f9a10a46ba09e04c8f169d67c2b9483db
Farana-Naz-Tultul <56032729+Farana-Naz-Tultul@users.noreply.github.com>
2022-03-24 00:04:40 -0400

    2nd push

--- app/src/main/java/com/hfad/tasks/TasksViewModel.kt
+++ app/src/main/java/com/hfad/tasks/TasksViewModel.kt
@@ -1,6 +1,6 @@
 package com.hfad.tasks
 
-import androidx.lifecycle.Transformations
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,10 +8,8 @@ import kotlinx.coroutines.launch
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""
 
-    private val tasks = dao.getAll()
-    val tasksString = Transformations.map(tasks) {
-            tasks -> formatTasks(tasks)
-    }
+    val tasks = dao.getAll()
+
 
     fun addTask() {
         viewModelScope.launch {
@@ -21,15 +19,4 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
         }
     }
 
-    fun formatTasks(tasks: List<Task>): String {
-        return tasks.fold("") {
-                str, item -> str + '\n' + formatTask(item)
-        }
-    }
-    fun formatTask(task: Task): String {
-        var str = "ID: ${task.taskId}"
-        str += '\n' + "Name: ${task.taskName}"
-        str += '\n' + "Complete: ${task.taskDone}" + '\n'
-        return str
-    }
 }
